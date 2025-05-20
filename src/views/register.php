824c0c56462cd3d6480db526752bc922b078c7a5
Kurt <trythisnet03@gmail.com>
2025-05-20 23:04:21 +0800

    -fixes some confusions

---
+++ src/views/register.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login to LostLink</title>
+</head>
+<body>
+    <h2>Register</h2>
+    <input type="text" name="fname" placeholder="Enter first name">
+    <input type="text" name="lname" placeholder="Enter last name">
+    <input type="email" name="email" placeholder="Enter your email">
+    <input type="number" name="phonenum" placeholder="Enter phone number">
+    <input type="password" name="password" placeholder="Enter password">
+    <input type="password" name="twopass" placeholder="Re-enter password">
+    <button onclick="register()">Submit</button>
+
+    <script>
+        function register(){
+
+        }
+    </script>
+</body>
+</html>
