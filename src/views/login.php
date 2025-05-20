14339f77faecbcec02be43c2f881e978c8e6ee00
Kurt <trythisnet03@gmail.com>
2025-05-20 22:54:02 +0800

    - change some thing

---
+++ src/views/login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login to LostLink</title>
+</head>
+<body>
+    <h2>Login</h2>
+    <input type="text" name="fname" placeholder="Enter first name">
+    <input type="text" name="lname" placeholder="Enter last name">
+    <input type="email" name="email" placeholder="Enter your email">
+    <input type="number" name="phonenum" placeholder="Enter phone number">
+</body>
+</html>
