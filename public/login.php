ffacb0a8f676ed68328eb5ba4f56be28441de098
Kurt <trythisnet03@gmail.com>
2025-05-23 15:24:30 +0800

    - added route

---
+++ public/login.php
@@ -0,0 +1,10 @@
+<?php
+    require_once __DIR__ . '/../src/controller/usersController.php';
+    $user = new UserController();
+
+    if($_SERVER['REQUEST_METHOD'] == "POST"){
+        $user->login();
+    }else{
+        $user->showLoginForm();
+    }
+?>
