ef868526009443758a154c6b67ddd382b67140b1
Kurt <trythisnet03@gmail.com>
2025-05-29 20:54:49 +0800

    - added nav bar

---
+++ src/views/nav.php
@@ -0,0 +1,13 @@
+<nav>
+    <div>
+        <ul>
+            <li>
+                <a href="">Home</a>
+                <a href="/public/profile.php">Report Item</a>
+                <a href="">View Recent Post</a>
+                <a href="/public/login.php">Login</a>
+                <a href="">Get Started</a>
+            </li>
+        </ul>
+    </div>
+</nav>
